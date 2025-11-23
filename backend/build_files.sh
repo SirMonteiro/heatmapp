d981628c963db22cd044a7c75d85da26e3ba2e84
Gabriel Monteiro de Souza <gabrielmonteirodesouza@tutanota.com>
2025-11-23 12:56:49 -0300

    feat: add popular_icones to vercel script and github releases apk ref

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -7,5 +7,8 @@ echo "Make migration"
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
+echo "Populate icons"
+python3 manage.py popular_icones
+
 echo "Collect static files"
 python3 manage.py collectstatic --noinput --clear
